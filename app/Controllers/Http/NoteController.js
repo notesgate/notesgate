7e5b1fdd634a15fb2ee6cc4c404bcb3fd4d745eb
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2019-05-02 00:21:56 +0700

    add new data field

--- app/Controllers/Http/NoteController.js
+++ app/Controllers/Http/NoteController.js
@@ -26,11 +26,20 @@ class NoteController {
 		return view.render('user.home');
 	}
 
-	async save({ request, response }) {
+	async save({ auth, request, response }) {
 
 		// const data = await Note.find()
 		// console.log("data", data)
-		const note = new Note(request.body)
+		let data = {
+			title: request.body.title,
+			lable: request.body.lable,
+			desc: request.body.desc,
+			section: request.body.section,
+			user_id: auth.user._id
+		}
+		// data.user_id = ObjectId(auth.user._id)
+		console.log("sss", data)
+		const note = new Note(data)
 		note.save()
 		// await note.save()
 		// const proc = await Note.where({ "zikri": { $exists: true, $not: null } })
