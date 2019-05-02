8b16dd0b2df8da5da8a1486fc40aacffc8a372f5
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2019-05-02 07:26:45 +0700

    edit note controller

--- app/Controllers/Http/NoteController.js
+++ app/Controllers/Http/NoteController.js
@@ -3,27 +3,63 @@ const Note = use("App/Models/Note")
 class NoteController {
 
 	async write({
+		params,
 		view
 	}) {
-		return view.render('user.write');
+		console.log("params", params.note);
+
+		if (params.note) {
+			console.log("edit true");
+
+			const note = await Note.where({ _id: params.note }).fetch()
+			let data = note.toJSON();
+			data[0].lable = data[0].lable.substr(1, data[0].lable.length - 2).split(',')
+			console.log(data);
+
+			return view.render('user.write', { note: data });
+		} else {
+			console.log("edit false");
+
+			return view.render('user.write');
+		}
 	}
 
 	async read({
+		auth,
+		params,
+		request,
+		response,
 		view
 	}) {
-		return view.render('user.note');
+
+		const note = await Note.where({ _id: params.note }).fetch()
+		let data = note.toJSON();
+		data[0].lable = data[0].lable.substr(1, data[0].lable.length - 2).split(',')
+		// console.log(data);
+		data[0].own = "false"
+
+		if (data[0].user_id == auth.user._id) {
+			data[0].own = "true"
+		}
+		return view.render('user.note', { note: data });
 	}
 
 	async get({
 		auth, view
 	}) {
-		return view.render('user.home');
+		const note = await Note.all()
+		// console.log("getnote", note);
+
+		return view.render('user.home', { 'note': 's' });
 	}
 
 	async all({
 		view
 	}) {
-		return view.render('user.home');
+		const note = await Note.all()
+		const data = note.toJSON();
+		// console.log("getnote", data);
+		return view.render('user.home', { notes: data });
 	}
 
 	async save({ auth, request, response }) {
