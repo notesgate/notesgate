740d78594a50a8e085a88c94511bb7c8cc8a15bf
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2019-05-24 09:31:46 +0700

    add new editor menu

--- public/assets/components/right-menu/js/right-menu.js
+++ public/assets/components/right-menu/js/right-menu.js
@@ -1,13 +1,7 @@
 $(document).ready(function () {
 	var count=0;
 	$('.menu-editor').on('click', function (e) {
-		if($($(e.target).closest('DIV')).data('flag')=="open"){
-			$($(e.target).closest('DIV')).removeClass('open');
-			$($(e.target).closest('DIV')).data('flag',"close");
-		}else{
-			$($(e.target).closest('DIV')).addClass('open');
-			$($(e.target).closest('DIV')).data('flag',"open")
-		}
+
 		
 	});
 });
