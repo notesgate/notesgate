284ba10b9b27b9a47deae9c4d525c407c0702882
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2019-06-04 12:32:55 +0700

    rebase init

---
+++ public/core/markdown/js/init.js
@@ -0,0 +1,12 @@
+var converter = new showdown.Converter(),
+    text      = '# hello, markdown!',
+    html      = converter.makeHtml(text);
+console.log(html);
+
+$(document).ready(function () {
+    let init=document.getElementsByClassName('init');
+    for (let index = 0; index < init.length; index++) {
+        init[index].previousElementSibling.innerHTML=init[index].getAttribute('data-val');
+        // init[index].parentElement.appendChild(el);
+    }
+});
