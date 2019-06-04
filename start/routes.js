2771df20983e2aa43cd2a30fc4770d8964c835fb
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2019-06-04 12:28:11 +0700

    add helper upload

--- start/routes.js
+++ start/routes.js
@@ -15,18 +15,22 @@
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
+const Helpers = use('Helpers')
 
-Route.get('/', 'NoteController.all').as('notesGate')
+Route.get('/', 'NoteController.all').as('notesGate').middleware(['user'])
 Route.get('/board', 'BoardController.getAllBoard').as('board').middleware(['user'])
 Route.get('/write', 'NoteController.writeNote').as('write').middleware(['user'])
 
 Route.group(() => {
 	Route.get('/write/:note', 'NoteController.write')
-	Route.get('/write/', 'NoteController.write').as('ww')
+	Route.get('/write/', 'NoteController.write')
+	Route.post('/write/', 'NoteController.write')
+	
 	Route.get('/read/:note', 'NoteController.read')
 	Route.get('/get', 'NoteController.get')
 	Route.get('/all', 'NoteController.all')
 	Route.post('/save', 'NoteController.save')
+	Route.post('/upload', 'NoteController.upload')
 }).prefix('/note').middleware(['user'])
 
 Route.group(() => {
